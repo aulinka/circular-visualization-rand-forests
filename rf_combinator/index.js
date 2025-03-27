05e9df04f280b16121e242bd78c02c99c3c98fd0
Laura Mikušová <laura.mikusova27@gmail.com>
2025-03-27 18:46:27 +0100

    fix indent

--- rf_combinator/index.js
+++ rf_combinator/index.js
@@ -3,10 +3,10 @@ import { Parser } from "./src/parser.js";
 import Combinator from "./src/combinator.js";
 
 async function main() {
-    const parser = new Parser();
-    const rf = parser.parse(await fs.readFile('../rf_processor/data.json', 'utf-8'));
-    const combinator = new Combinator();
-    combinator.combine(rf);
-    await fs.writeFile('tree.json', JSON.stringify(rf, null, 2));
+  const parser = new Parser();
+  const rf = parser.parse(await fs.readFile('../rf_processor/data.json', 'utf-8'));
+  const combinator = new Combinator();
+  combinator.combine(rf);
+  await fs.writeFile('tree.json', JSON.stringify(rf, null, 2));
 }
 main();
--- rf_combinator/src/parser.js
+++ rf_combinator/src/parser.js
@@ -5,70 +5,70 @@ import utils from "rf_shared/src/utils.js";
 const NODE_IS_LEAF = -1;
 
 export class Parser {
-    #finalForest = new RandomForest();
-    #forest = null;
-    /** @type {?Tree} */
-    #finalTree = null;
-    #rawTree = null;
+  #finalForest = new RandomForest();
+  #forest = null;
+  /** @type {?Tree} */
+  #finalTree = null;
+  #rawTree = null;
 
-    constructor() {
+  constructor() {
 
+  }
+
+  parse(rfProcData) {
+    this.#forest = JSON.parse(rfProcData);
+    this.#finalForest = new RandomForest();
+
+    this.#finalForest.info.model = this.#forest.config.model;
+    this.#finalForest.info.randomState = this.#forest.config.random_state;
+    this.#finalForest.info.testSize = this.#forest.config.test_size;
+    this.#finalForest.info.accuracy = this.#forest.accuracy;
+
+    for (const [ key, val ] of this.#forest.feature_names.entries()) {
+      this.#finalForest.features.push(new Feature(parseInt(key), val));
+    }
+    for (const [ key, val ] of this.#forest.target_names.entries()) {
+      this.#finalForest.targets.push(new Target(parseInt(key), val));
     }
 
-    parse(rfProcData) {
-        this.#forest = JSON.parse(rfProcData);
-        this.#finalForest = new RandomForest();
-
-        this.#finalForest.info.model = this.#forest.config.model;
-        this.#finalForest.info.randomState = this.#forest.config.random_state;
-        this.#finalForest.info.testSize = this.#forest.config.test_size;
-        this.#finalForest.info.accuracy = this.#forest.accuracy;
-
-        for (const [ key, val ] of this.#forest.feature_names.entries()) {
-            this.#finalForest.features.push(new Feature(parseInt(key), val));
-        }
-        for (const [ key, val ] of this.#forest.target_names.entries()) {
-            this.#finalForest.targets.push(new Target(parseInt(key), val));
-        }
-
-        for (const [ id, tree ] of this.#forest.trees.entries()) {
-            this.#parseTree(tree, id);
-        }
-        return this.#finalForest;
+    for (const [ id, tree ] of this.#forest.trees.entries()) {
+      this.#parseTree(tree, id);
     }
+    return this.#finalForest;
+  }
 
-    #parseTree(rawTree, treeId) {
-        this.#rawTree = rawTree;
-        const tree = new Tree();
-        tree.uid = treeId;
-        this.#finalTree = tree;
-        this.#finalForest.addTree(tree);
+  #parseTree(rawTree, treeId) {
+    this.#rawTree = rawTree;
+    const tree = new Tree();
+    tree.uid = treeId;
+    this.#finalTree = tree;
+    this.#finalForest.addTree(tree);
 
-        this.#parseNode(0, 0);
+    this.#parseNode(0, 0);
+  }
+
+  #parseNode(tid, depth) {
+    const node = new Node();
+    this.#finalForest.addNode(node);
+    this.#finalTree.addNode(node);
+    node.treeNodeId = tid;
+    const isLeaf = this.#rawTree['children_left'][tid] == NODE_IS_LEAF;
+    const isRootNode = depth == 0;
+    node.depth = depth;
+    node.values = this.#rawTree.values[tid][0];
+
+    if (!isLeaf) {
+      node.feature = this.#finalForest.features[this.#rawTree['features'][tid]];
+      node.featureThreshold = this.#rawTree['threshold'][tid];
+
+      node.left = this.#parseNode(this.#rawTree['children_left'][tid], depth + 1);
+      node.left.parent = node;
+      node.right = this.#parseNode(this.#rawTree['children_right'][tid], depth + 1);
+      node.right.parent = node;
+    } else {
+      const targetId = node.values.indexOf(Math.max(...node.values));
+      node.target = this.#finalForest.targets[targetId];
     }
-
-    #parseNode(tid, depth) {
-        const node = new Node();
-        this.#finalForest.addNode(node);
-        this.#finalTree.addNode(node);
-        node.treeNodeId = tid;
-        const isLeaf = this.#rawTree['children_left'][tid] == NODE_IS_LEAF;
-        const isRootNode = depth == 0;
-        node.depth = depth;
-        node.values = this.#rawTree.values[tid][0];
-
-        if (!isLeaf) {
-            node.feature = this.#finalForest.features[this.#rawTree['features'][tid]];
-            node.featureThreshold = this.#rawTree['threshold'][tid];
-
-            node.left = this.#parseNode(this.#rawTree['children_left'][tid], depth + 1);
-            node.left.parent = node;
-            node.right = this.#parseNode(this.#rawTree['children_right'][tid], depth + 1);
-            node.right.parent = node;
-        } else {
-            const targetId = node.values.indexOf(Math.max(...node.values));
-            node.target = this.#finalForest.targets[targetId];
-        }
-        return node;
-    }
+    return node;
+  }
 }
